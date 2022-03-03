dbe06a20c825623845b31e5c8f710ced51547f5e
Luis Rodriguez <lalo_luis_6@hotmail.com>
2022-03-03 09:52:42 -0600

    adding graph functions

--- js/ArduinoSerial.js
+++ js/ArduinoSerial.js
@@ -96,6 +96,7 @@ class ArduinoSerial{
                     }
                 } )
             });
+            console.log(arrayFinal);
             // Enviar datos al servidor por web sockets
             socket.emit(servidor.sockets.intercambiarDatos, arrayFinal);
         })
--- public/index.js
+++ public/index.js
@@ -1,6 +1,7 @@
 const socket = io();
 const html = new HTMLManager(socket);
 
+
 html.botonConectarConArduino();
 
 html.activarBotonComenzar(false)
@@ -11,12 +12,56 @@ socket.on('connect', ()=>{
 socket.on('arduinoConnectionState', data => {
     html.activarBotonComenzar(data.isConnected)
     if (data.isConnected) {
-        html.ocultarTodoExcepto(2, ".Contenido_Estado")
+        html.ocultarTodoExcepto(2, ".Contenido_Estado");
     } else {
-        html.ocultarTodoExcepto(0, ".Contenido_Estado")
+        html.ocultarTodoExcepto(0, ".Contenido_Estado");
     }
 })
 
-socket.on('data', html.asignaDatos)
+const sensor1 = document.getElementById('sensor1');
+
+const A = [[],[],[],[],[]];
+const B = [];
+socket.on('data', data => {
+    html.asignaDatos(data);
+    data.forEach( (one, index) => {
+        A[index].push(one);
+        if (A[0].length > 25) {
+            A[index].shift();
+        }
+    } )
+
+    if (B.length < 25) {
+        B.push(A[0].length);
+        // B.shift();
+    }
+
+
+    const layout = {
+        title: 'Datos',
+        xaxis: {
+          title: 'Numero',
+        },
+        yaxis: {
+          title: 'Valor',
+        },
+        margin: { t: 0 },
+        
+      };
+    
+    Plotly.newPlot( sensor1, [{
+        x: B,
+        y: A[0],
+    }], layout );
+
+    var update = {
+        'xaxis.range': [0, 25],   // updates the xaxis range
+        'yaxis.range': [0, 1024]     // updates the end of the yaxis range
+    };
+    Plotly.relayout( sensor1, update)
+});
+
 
 html.activarForms();
+
+
