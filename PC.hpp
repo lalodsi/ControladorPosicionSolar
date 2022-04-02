a70f36c3e0b9f2d069844ba9e62e7ce5d58c3563
Luis Rodriguez <lalo_luis_6@hotmail.com>
2022-04-01 18:05:57 -0600

    create PC and position classes

---
+++ PC.hpp
@@ -0,0 +1,22 @@
+#include "external.hpp"
+
+class PC: public external
+{
+private:
+    void receive(string data){
+        //
+    }
+
+public:
+    void init(int baudRate = 115200){
+        Serial.begin(baudRate);
+        pins = 0;
+    }
+
+    /**
+     * Envia información a la computadora
+     */
+    void send(){
+        // 
+    }
+};
---
+++ Position.hpp
@@ -0,0 +1,34 @@
+/**
+ * @brief Clase position
+ * 
+ */
+class position
+{
+public:
+    // Constructores
+    /**
+     * @brief Construct a new position object
+     * 
+     */
+    position(){
+        azimutAngle = 0;
+        inclinationAngle = 0;
+    }
+    /**
+     * @brief Construct a new position object
+     * 
+     * @param azimut 
+     * @param inclination 
+     */
+    position(double azimut, double inclination){
+        azimutAngle = azimut;
+        inclinationAngle = inclination;
+    }
+
+    double azimutAngle;
+    double inclinationAngle;
+
+    void getAzimutDeg(){
+
+    }
+};
