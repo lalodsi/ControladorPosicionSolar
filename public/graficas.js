bc24359f40ae4a3457bb8b97d5aae4f5578f5651
Luis Rodriguez <lalo_luis_6@hotmail.com>
2022-04-05 16:15:15 -0500

    First 3d plot working with correct layout

--- public/graficas.js
+++ public/graficas.js
@@ -1,5 +1,5 @@
 class graficas{
-    layout = {
+    layout2D = {
         title: 'Datos',
         yaxis: {
             title: 'Valor',
@@ -14,15 +14,44 @@ class graficas{
         },
         showlegend: false
     };
+    layout3D = {
+        scene1: {
+            annotations: {
+                text: 'Hola',
+                ax: 0,
+                ay: 0
+            },
+            // paper_bgcolor: "#00000000",
+            // plot_bgcolor: "#00000000",
+            xaxis: {
+                range: [-1, 1]
+            },
+            yaxis: {
+                range: [-1, 1]
+            },
+            zaxis: {
+                range: [-1, 1]
+            },
+        },
+        // height: 600,
+        margin: {
+            l: 0,
+            r: 0,
+            b: 0,
+            t: 0,
+            pad: 0
+        },
+    };
     isPlotted = false;
 
     constructor(title){
-        this.layout.title = title;
+        this.layout2D.title = title;
+        // this.title = type;
     }
 
     draw = function (id, data) {
         if (!this.isPlotted) {
-            Plotly.newPlot( id, data, this.layout , {staticPlot: true});
+            Plotly.newPlot( id, data, this.layout2D , {staticPlot: true});
             this.isPlotted = true;
         }
         else{
@@ -32,6 +61,23 @@ class graficas{
             });
             Plotly.relayout( id, data[0]);
         }
+    }
+    
+    /**
+     * 
+     * @param {html element} id objeto html con el div que contiene a la grafica
+     * @param {*} data 
+     */
+    draw3d = function (id, data) {
+        if (!this.isPlotted) {
+            Plotly.newPlot( id, data, this.layout3D);
+            this.isPlotted = true;
+        }
+        else{
+            // Plotly.animate( id, data );
+            // Plotly.react( id, data);
+        }
+        
     }
 
 }
--- public/index.js
+++ public/index.js
@@ -18,8 +18,8 @@ socket.on('arduinoConnectionState', data => {
     }
 })
 
-const sensores = document.getElementById('sensor1');
-const grafica1 = new graficas("Sensores");
+
+
 
 
 const trazo1 = {
@@ -42,14 +42,62 @@ const trazo5 = {
     x: [],
     y: []
 }
+const homeTrace = {
+    x: [0, 1],
+    y: [0, 1],
+    z: [0, 1],
+	marker: {
+		size: 8,
+		line: {
+		    color: 'rgba(217, 217, 217, 0.14)',
+		    width: 0.5
+        },
+		opacity: 0.8
+    },
+    line: {
+        width: 10,
+        color: [2],
+        colorscale: "Viridis"
+    },
+    type: 'scatter3d',
+    scene: 'scene1',
+    // xaxis: {
+    //     range: [-1, 1]
+    // },
+    // yaxis: {
+    //     range: [-1, 1]
+    // },
+    // zaxis: {
+    //     range: [-1, 1]
+    // },
+};
+
+
+
+let dirX, dirY, dirZ;
+    const graficaSensores = new graficas("Sensores");
+    const representacion3D = new graficas("robot3d");
 
 socket.on('data', data => {
+    limitarCantidadDatosRecibidos = function(grafica, data) {
+        const CANTIDAD_DE_ELEMENTOS_MAXIMA = 25;
+        grafica.y.push(data);
+        
+        if (grafica.y.length > CANTIDAD_DE_ELEMENTOS_MAXIMA) {
+            grafica.y.shift();
+        }
+        if (grafica.x.length < CANTIDAD_DE_ELEMENTOS_MAXIMA) {
+            grafica.x.push(grafica.y.length);
+        }
+    }
+
+    
     html.asignaDatos(data);
-    limitarA25(trazo1, data[0])
-    limitarA25(trazo2, data[1])
-    limitarA25(trazo3, data[2])
-    limitarA25(trazo4, data[3])
-    limitarA25(trazo5, data[4])
+    limitarCantidadDatosRecibidos(trazo1, data[0])
+    limitarCantidadDatosRecibidos(trazo2, data[1])
+    limitarCantidadDatosRecibidos(trazo3, data[2])
+    limitarCantidadDatosRecibidos(trazo4, data[3])
+    limitarCantidadDatosRecibidos(trazo5, data[4])
     const datos = [
         trazo1,
         trazo2,
@@ -57,20 +105,20 @@ socket.on('data', data => {
         trazo4,
         trazo5
     ]
-    grafica1.draw(sensores, datos);
+    dirX = data[0];
+    dirY = data[1];
+    dirZ = data[2];
+    homeTrace.x[1] = (dirX - 512) / 512;
+    homeTrace.y[1] = (dirY - 512) / 512;
+    homeTrace.z[1] = (dirZ - 512) / 512;
+    
+    
+
+    graficaSensores.draw("sensores", datos);
+    representacion3D.draw3d("robot3d", [homeTrace]);
+    
 });
 
-function limitarA25(grafica, data) {
-    grafica.y.push(data);
-    
-    if (grafica.y.length > 25) {
-        grafica.y.shift();
-    }
-    if (grafica.x.length < 25) {
-        grafica.x.push(grafica.y.length);
-    }
-
-}
 
 html.activarForms();
 
