523fbe297eab5029999c762a718dd0cbe9b9fd56
Luis Rodriguez <lalo_luis_6@hotmail.com>
2022-06-10 22:43:55 -0500

    added functions for receive data continuously

---
+++ motor.h
@@ -0,0 +1,71 @@
+void Mover(int *marcha, int *ciclos, int *pasos, int direccion){
+  *marcha = *marcha + 1;
+  *ciclos = *marcha / 4;
+  *pasos = (*marcha - (*ciclos * 4)) + 1;
+
+  if (direccion < 0)
+  {
+    *pasos = 5 - *pasos;
+  }
+}
+void moverMotor(int pasos, int input1, int input2, int input3, int input4){
+  switch (pasos)
+  {
+  case 1:
+    digitalWrite(input1, HIGH);
+    digitalWrite(input2, LOW);
+    digitalWrite(input3, LOW);
+    digitalWrite(input4, HIGH);
+    break;
+  
+  case 2:
+    digitalWrite(input1, HIGH);
+    digitalWrite(input2, LOW);
+    digitalWrite(input3, HIGH);
+    digitalWrite(input4, LOW);
+    break;
+  
+  case 3:
+    digitalWrite(input1, LOW);
+    digitalWrite(input2, HIGH);
+    digitalWrite(input3, HIGH);
+    digitalWrite(input4, LOW);
+    break;
+  
+  case 4:
+    digitalWrite(input1, LOW);
+    digitalWrite(input2, HIGH);
+    digitalWrite(input3, LOW);
+    digitalWrite(input4, HIGH);
+    break;
+  
+  default:
+    digitalWrite(input1, LOW);
+    digitalWrite(input2, LOW);
+    digitalWrite(input3, LOW);
+    digitalWrite(input4, LOW);
+    break;
+  }
+}
+/**
+ * @brief Se moverá el motor para la elevación del sistema
+ * 
+ * @param direccion numero entero que representa la dirección a donde el motor estará moviendose
+ */
+void moverY(int direccion){
+
+  Mover(&marchaElevacion, &ciclosElevacion, &pasosElevacion, direccion);
+  
+  moverMotor(pasosElevacion, 1, 2, 3, 4);
+  
+}
+/**
+ * @brief Se moverá el motor para la rotación del sistema
+ * 
+ */
+void moverX(int direccion){
+  
+  Mover(&marchaRotacion, &ciclosRotacion, &pasosRotacion, direccion);
+  
+  moverMotor(pasosRotacion, 5, 6, 7, 8);
+}
--- tesis.ino
+++ tesis.ino
@@ -1,5 +1,6 @@
 #include "sensor.hpp"
 #include "math.h"
+#include "motor.h"
 
 // Declaración de sensores externos
 sensor sensor1(A0);
@@ -101,77 +102,6 @@ void SPL_algorithm(){
   }
   
 }
-void Mover(int *marcha, int *ciclos, int *pasos, int direccion){
-  *marcha = *marcha + 1;
-  *ciclos = *marcha / 4;
-  *pasos = (*marcha - (*ciclos * 4)) + 1;
-
-  if (direccion < 0)
-  {
-    *pasos = 5 - *pasos;
-  }
-}
-void moverMotor(int pasos, int input1, int input2, int input3, int input4){
-  switch (pasos)
-  {
-  case 1:
-    digitalWrite(input1, HIGH);
-    digitalWrite(input2, LOW);
-    digitalWrite(input3, LOW);
-    digitalWrite(input4, HIGH);
-    break;
-  
-  case 2:
-    digitalWrite(input1, HIGH);
-    digitalWrite(input2, LOW);
-    digitalWrite(input3, HIGH);
-    digitalWrite(input4, LOW);
-    break;
-  
-  case 3:
-    digitalWrite(input1, LOW);
-    digitalWrite(input2, HIGH);
-    digitalWrite(input3, HIGH);
-    digitalWrite(input4, LOW);
-    break;
-  
-  case 4:
-    digitalWrite(input1, LOW);
-    digitalWrite(input2, HIGH);
-    digitalWrite(input3, LOW);
-    digitalWrite(input4, HIGH);
-    break;
-  
-  default:
-    digitalWrite(input1, LOW);
-    digitalWrite(input2, LOW);
-    digitalWrite(input3, LOW);
-    digitalWrite(input4, LOW);
-    break;
-  }
-}
-/**
- * @brief Se moverá el motor para la elevación del sistema
- * 
- * @param direccion numero entero que representa la dirección a donde el motor estará moviendose
- */
-void moverY(int direccion){
-
-  Mover(&marchaElevacion, &ciclosElevacion, &pasosElevacion, &direccion);
-  
-  moverMotor(pasosElevacion, 1, 2, 3, 4);
-  
-}
-/**
- * @brief Se moverá el motor para la rotación del sistema
- * 
- */
-void moverX(int direccion){
-  
-  Mover(&marchaRotacion, &ciclosRotacion, &pasosRotacion, &direccion);
-  
-  moverMotor(pasosRotacion, 5, 6, 7, 8);
-}
 
 /**
  * @brief Esperará a que haya información en el puerto serie para continuar la ejecución
@@ -213,11 +143,27 @@ void calibrar(){
 }
 
 void controlar(){
-  // Señal de entrada
-  Serial.println("ok");
-  // Controlar
-    Serial.println("Ciclo de control");
-  // while (true){
-  // }
+  while (true){
+    waitForSerial();
+    entrada = Serial.readString();
+    if (entrada.equals("salir"))
+      break;
+    int n = entrada.indexOf(","); // Separador para el valor de X y de Y
+    String rotacionTexto = entrada.substring(0, n);
+    String elevacionTexto = entrada.substring(n + 1);
+    // Movimiento
+    int rotacion = rotacionTexto.toInt();
+    int elevacion = elevacionTexto.toInt();
+    // Si faltan más pasos, realizarlos
+    int pasosRestantesRotacion = abs(rotacion - pasosRotacion);
+    int pasosRestantesElevacion = abs(elevacion - pasosElevacion);
+    while (pasosRestantesElevacion > 0 || pasosRestantesRotacion > 0)
+    {
+      moverX(rotacion - pasosRotacion);
+      moverY(elevacion - pasosElevacion);
+    }
+    delay(100);
+    // Serial.println("La entrada es " + x + "," + y);
+  }
   
 }
