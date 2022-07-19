cbd9d92100d48797881c59cf442fc3aa814fa02c
Luis Rodriguez <lalo_luis_6@hotmail.com>
2022-07-19 12:49:36 -0500

    serial monitor working

--- public/js/monitorSerial.js
+++ public/js/monitorSerial.js
@@ -2,6 +2,10 @@ const botonEnviarMonitorSerial = function () {
     const boton = document.getElementById('monitorSerialEnviar');
     boton.addEventListener('click', ()=>{
         const texto = document.getElementById("monitorSerialEnviarTexto").value;
+        const serialContenedor = document.getElementById('contenedorSerial');
+        const textoHTML = document.createElement('p');
+        textoHTML.innerHTML = `> ${texto}`;
+        serialContenedor.appendChild(textoHTML);
         socket.emit(
             eventos.enviarPalabra,
             {
