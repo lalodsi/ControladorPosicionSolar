2b37915b3aabf2546dedb734dcca5207bc44f427
Luis E. Rodriguez Ramirez <luis.c.rodriguez@capgemini.com>
2022-10-22 21:42:06 -0500

    Erased script and code not used

--- apps/UI/js/pruebaServer.js
+++
@@ -1,8 +0,0 @@
-const ArduinoSerial = require('./ArduinoSerial.js')
-const DOM = require('./DOM.js')
-
-let arduino = new ArduinoSerial()
-
-arduino.init()
-arduino.openPort()
-arduino.receiveData()
