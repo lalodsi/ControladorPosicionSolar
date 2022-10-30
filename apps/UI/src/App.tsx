1f5c600320136912272d57b19e9744f83f957d5f
Luís Eduardo Rodríguez Ramírez <LRODR250@ford.com>
2022-10-29 22:23:26 -0500

    Added typescript to the project

---
+++ apps/UI/src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const App: React.FC = () => {
+
+
+    return (
+        <h1>Hola mundo</h1>
+    )
+}
+
+export default App
--- apps/UI/src/index.js
+++
@@ -1,6 +0,0 @@
-import ReactDOM from "react-dom"
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <h1>Hola mundo</h1>
-)
