f961ba516f015f8cdf141a7f7b91c8ec33a38d06
Luis Eduardo Rodríguez Ramírez <LRODR250@ford.com>
2022-11-13 01:11:26 -0600

    Added new scripts and react-scripts

---
+++ apps/UI/public/main.js
@@ -0,0 +1,38 @@
+const { app, BrowserWindow } = require("electron");
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 1200,
+        height: 700,
+        frame: false,
+        titleBarStyle: 'hidden',
+        resizable: false,
+        webPreferences: {
+            // preload: path.join(__dirname, '../public/preload.js'),
+            nodeIntegration: true,
+            contextIsolation: true,
+            devTools: true,
+        }
+    })
+    // ipcMain.on('closeApp', ()=> {
+    //     win.close();
+    // })
+    // ipcMain.on('minimizeApp', ()=> {
+    //     win.minimize();
+    // })
+    win.loadURL(`http://localhost:3000`);
+};
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+})
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow()
+    }
+})
