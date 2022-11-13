295cd2bc320ff595405710cb4786c46a6ec447b2
Luis Eduardo Rodríguez Ramírez <LRODR250@ford.com>
2022-11-13 04:07:25 -0600

    Improve styles

--- apps/UI/src/App.tsx
+++ apps/UI/src/App.tsx
@@ -1,11 +1,23 @@
 import React from "react"
+import Button from "./components/core/button"
 import TopBar from "./sections/topbar"
 import "./styles.css"
+import { buttonTypes } from "./components/core/button"
+import MainContent from "./sections/mainContent"
 
 const App: React.FC = () => {
+    const pruebaBoton = () => {
+        console.log("Hola");
+    }
 
     return (
-        <TopBar />
+        <React.Fragment>
+            <TopBar />
+            <MainContent>
+                Hola
+                <Button text="Boton 1" handleClick={pruebaBoton} className={buttonTypes.NavigationButton} />
+            </MainContent>
+        </React.Fragment>
     )
 }
 
