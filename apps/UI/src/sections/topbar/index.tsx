29a2ab38fb410cb4b9e0baabbc1dd97c9fd5ee16
Luís Eduardo Rodríguez Ramírez <rodriguezramirezluiseduardo0@gmail.com>
2022-11-12 22:44:26 -0600

    Added TopBar

---
+++ apps/UI/src/sections/topbar/index.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import "./styles.css"
+
+function TopBar() {
+    return ( 
+        <div className="topBar">
+            <div className='titleBar'>
+                <img src="" alt="" />
+                <div className='title'>
+                    Tesis
+                </div>
+            </div>
+            <div className='titleBarBtns'></div>
+        </div>
+     );
+}
+
+export default TopBar;
