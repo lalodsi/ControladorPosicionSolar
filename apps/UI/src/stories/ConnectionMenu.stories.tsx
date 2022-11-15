489b5bba263bb416b024472b272c868cafb0ad58
Luis Eduardo Rodríguez Ramírez <LRODR250@ford.com>
2022-11-14 19:08:22 -0600

    Added comection Menu Story

---
+++ apps/UI/src/stories/ConnectionMenu.stories.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import ConnectionMenu from '../components/principal/connectionMenu';
+
+export default {
+  title: 'Connection Menu',
+  component: ConnectionMenu,
+  parameters: {
+    // More on Story layout: https://storybook.js.org/docs/react/configure/story-layout
+    layout: 'fullscreen',
+  },
+} as ComponentMeta<typeof ConnectionMenu>;
+
+const Template: ComponentStory<typeof ConnectionMenu> = (args) => <ConnectionMenu {...args} />;
+
+export const Normal = Template.bind({});
+Normal.args = {
+    conectado : false,
+    esperando : false
+};
