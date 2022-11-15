ce926e1c581252c2fa5a5ae1b7e8bda53a824e86
Luis Eduardo Rodríguez Ramírez <LRODR250@ford.com>
2022-11-14 19:09:28 -0600

    Developing connection menu

---
+++ apps/UI/src/components/principal/connectionMenu/index.tsx
@@ -0,0 +1,62 @@
+import * as React from 'react';
+import Button, { buttonTypes } from '../../core/button';
+import "./styles.css"
+
+interface IConnectionMenuProps {
+  conectado: boolean,
+  esperando: boolean
+}
+
+const ConnectionMenu: React.FunctionComponent<IConnectionMenuProps> = (props) => {
+  const {
+    conectado = false,
+    esperando = false
+  } = props;
+
+  const handleConnect = () => {
+    console.log("Conectar al arduino");
+    
+  }
+
+  return (
+    <section className="subsection estado" id="estadoConexion">
+      <div>
+          <div>Estado</div>
+          {
+            (!conectado && !esperando) &&
+            <div className="Contenido_Estado">
+              <div id="state" className="desconectado">Desconectado</div>
+              <div className="menuConectar">
+                <p className="info">Establecer una conexión con arduino</p>
+                <div className="contenedorPuertos">
+                  <select name="puerto" id="puertos" className="listaPuertos" aria-placeholder="">
+                      <option value="1">Elige un puerto</option>
+                  </select>
+                  <button className="botonActualizar" id="botonActualizar"></button>
+                </div>
+                {/* <button id="botonConectar" className="botonArduino conectar">Conectar</button> */}
+                <Button text='Conectar' className={buttonTypes.connectButton} handleClick={handleConnect} />
+              </div>
+            </div>
+          }
+          {
+            (!conectado && esperando) &&
+            <div className="Contenido_Estado">
+                <div id="state" className="conectando">Conectando...</div>
+            </div>
+          }
+          {
+            conectado &&
+            <div className="Contenido_Estado">
+                <div id="state" className="conectado">Conectado</div>
+                <button id="botonDesconectar" className="botonArduino desconectar">Desconectar</button>
+            </div>
+          }
+      </div>
+    </section>
+  );
+};
+
+
+
+export default ConnectionMenu;
