aa511dd863c5c80b5daf5e0ba93c11529034e7b2
Luis Eduardo Rodríguez Ramírez <LRODR250@ford.com>
2022-11-14 18:15:40 -0600

    Deleted test files

--- apps/UI/src/stories/Header.stories.tsx
+++ apps/UI/src/stories/Header.stories.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
-import { Header } from './Header';
 import TopBar from '../sections/topbar';
 
 export default {
